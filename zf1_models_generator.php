790dde29f0d3face1e391eca76bcefd501da95ef
Giovanni Ramos <giovannilauro@gmail.com>
2013-02-22 13:34:54 -0400

    Add New BSD License

--- zf1_models_generator.php
+++ zf1_models_generator.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the BSD License.
+ * 
+ * https://github.com/giovanniramos/zf_generate_models
+ *
+ * Copyright(c) 2013 by Giovanni Ramos. All rights reserved.
+ * 
+ * LICENSE New BSD License
+ *
+ */
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="en">
 <head>
