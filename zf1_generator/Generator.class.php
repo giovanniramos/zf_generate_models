87d8c5dda9cee52404d9690f83fa90bc1304e1c2
Giovanni Ramos <giovannilauro@gmail.com>
2013-04-26 08:59:52 -0400

    Added support to connect database via PDO

--- zf1_generator/Generator.class.php
+++ zf1_generator/Generator.class.php
@@ -12,28 +12,40 @@
 
 class Generator
 {
-    private static $adapter = 'mysql';
-    private static $link;
-    private static $databases;
-    private static $tables;
+    private static $dbh;
+    private static $adapter;
+    private static $hostname;
+    private static $username;
+    private static $password;
+    private static $database;
     private static $backup = true;
 
-    public static function adapter($adapter)
+    public static function connect($adapter, $hostname, $username, $password, $database)
     {
-        self::$adapter = $adapter;
-    }
+        self::setAdapter($adapter);
+        self::setParameters($hostname, $username, $password, $database);
 
-    public static function connect($hostname, $username, $password)
-    {
-        switch (self::$adapter) {
-            case 'MYSQL':
-            case 'PDO_MYSQL':
-                self::$link = @mysql_connect($hostname, $username, $password);
-                break;
-            default: exit('No connection!');
+        try {
+            switch (self::getAdapter()) {
+                case 'MYSQL':
+                case 'PDO_MYSQL':
+                    self::$dbh = new PDO('mysql:host=' . $hostname . ';dbname=' . $database, $username, $password);
+                    break;
+                default: exit('Adapter not implemented!');
+            }
+
+            self::$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $msg = mb_convert_encoding($e->getMessage(), 'utf8');
+            exit($msg);
         }
 
-        self::setDatabases();
+        return self::$dbh;
+    }
+
+    public static function reconnect($database)
+    {
+        self::connect(self::getAdapter(), self::$hostname, self::$username, self::$password, $database);
     }
 
     public static function setBackup($backup)
@@ -41,137 +53,136 @@ class Generator
         self::$backup = $backup;
     }
 
-    public static function setDatabases($database = null)
+    public static function setAdapter($adapter)
     {
-        if (!$database):
-            switch (self::$adapter):
-                case 'MYSQL':
-                case 'PDO_MYSQL':
-                    $dbs = self::list_dbs();
+        self::$adapter = $adapter;
+    }
 
-                    $database = null;
-                    while ($row = self::fetch_row($dbs)):
-                        if (in_array($row[0], array('information_schema', 'performance_schema', 'phpmyadmin', 'mysql', 'webauth')))
-                            continue;
-                        else
-                            $database[] = $row[0];
-                    endwhile;
-                    self::$databases = $database;
-                    break;
-            endswitch;
-        else:
-            switch (self::$adapter):
-                case 'MYSQL':
-                case 'PDO_MYSQL':
-                    $dbs = self::query('SHOW TABLES FROM ' . $database);
+    public static function getAdapter()
+    {
+        return self::$adapter;
+    }
 
-                    $tables = null;
-                    while ($row = self::fetch_row($dbs)):
-                        $tables[] = $row[0];
-                    endwhile;
-                    self::$tables = $tables;
-                    break;
-            endswitch;
-        endif;
+    public static function setParameters($hostname, $username, $password, $database)
+    {
+        self::$hostname = $hostname;
+        self::$username = $username;
+        self::$password = $password;
+        self::$database = basename($database);
+    }
+
+    public static function getDatabase()
+    {
+        return self::$database;
     }
 
     public static function getDatabases()
     {
-        return self::$databases;
+        $dbs = self::list_dbs();
+
+        $databases = null;
+        foreach ($dbs as $db)
+            $databases[] = basename(end($db));
+
+        return $databases;
     }
 
     public static function getTablesFromDatabase($database)
     {
-        Generator::setDatabases($database);
+        $tbs = self::list_tbs($database);
 
-        return self::$tables;
-    }
+        $tables = null;
+        foreach ($tbs as $tb)
+            $tables[] = end($tb);
 
-    public static function query($query)
-    {
-        switch (self::$adapter):
-            case 'MYSQL':
-            case 'PDO_MYSQL':
-                $result = mysql_query($query, self::$link);
-                break;
-        endswitch;
-
-        return $result;
-    }
-
-    public static function select_db($database)
-    {
-        switch (self::$adapter):
-            case 'MYSQL':
-            case 'PDO_MYSQL':
-                mysql_select_db($database, self::$link) or mysql_error();
-                break;
-        endswitch;
+        return $tables;
     }
 
     public static function list_dbs()
     {
-        switch (self::$adapter):
+        switch (self::getAdapter()):
             case 'MYSQL':
             case 'PDO_MYSQL':
-                $dbs = self::query('SHOW DATABASES');
+                $dbs = self::query("SHOW DATABASES WHERE `Database` NOT IN ('information_schema', 'performance_schema', 'phpmyadmin', 'mysql', 'webauth');", false);
                 break;
         endswitch;
 
         return $dbs;
     }
 
-    public static function num_fields($result)
+    public static function list_tbs($database)
     {
-        switch (self::$adapter):
+        if (is_null($database))
+            return;
+
+        switch (self::getAdapter()):
             case 'MYSQL':
             case 'PDO_MYSQL':
-                $num_fields = mysql_num_fields($result);
+                $tbs = self::query("SHOW TABLES FROM " . $database . ";", false);
                 break;
         endswitch;
 
-        return $num_fields;
+        return $tbs;
     }
 
-    public static function fetch_field($result, $field_offset = 0)
+    public static function fetch_fields($database, $table)
     {
-        switch (self::$adapter):
+        switch (self::getAdapter()):
             case 'MYSQL':
             case 'PDO_MYSQL':
-                $field = mysql_fetch_field($result, $field_offset);
+                $fields = self::query("SHOW COLUMNS FROM " . $database . "." . $table . ";", false);
+                $columns = array('Field', 'Type', 'Key', 'Null');
                 break;
         endswitch;
 
-        return $field;
+        foreach ($fields as $field):
+            $_name = $field[$columns[0]];
+            $_type = $field[$columns[1]];
+            $_pkey = $field[$columns[2]];
+            $_null = $field[$columns[3]];
+
+            if (preg_match('~^((int)(\([0-9]{1,2}\))?([\s].*)?|integer)$~i', $_type))
+                $_tpzf = 'int';
+            elseif (preg_match('~(date|datetime)~i', $_type))
+                $_tpzf = 'date';
+            elseif (preg_match('~(yes|0)~i', $_null))
+                $_tpzf = 'null';
+            else
+                $_tpzf = 'string';
+
+            $_fields[] = array(
+                '_name' => $_name,
+                '_type' => $_type,
+                '_tpzf' => $_tpzf,
+                '_pkey' => $_pkey,
+                '_null' => $_null
+            );
+        endforeach;
+
+        return $_fields;
     }
 
-    public static function fetch_row($result)
+    public static function query($query, $both = true)
     {
-        switch (self::$adapter):
-            case 'MYSQL':
-            case 'PDO_MYSQL':
-                $row = mysql_fetch_row($result);
-                break;
-        endswitch;
+        $query = self::$dbh->prepare($query);
+        $query->execute();
+        $rows = ($both) ? $query->fetchAll() : $query->fetchAll(PDO::FETCH_ASSOC);
 
-        return $row;
+        return $rows;
     }
 
     public static function generate($database, $tables)
     {
-        self::select_db($database);
-
         // Generate model layers
         echo '<fildset><legend>Generate models</legend>';
-        if (!$tables):
+        if (!$tables)
             exit('<div class="alert"><h4>No results</h4> <a href="javascript:history.back();" />Click here and try again</a></div>');
-        endif;
         foreach ($tables as $table)
-            self::gen_models($table);
+            self::gen_models($database, $table);
         echo '</fildset>';
     }
 
-    public static function gen_models($table = null)
+    public static function gen_models($database, $table)
     {
         // Checks if the following directories exist
         // DIR: \application
@@ -181,59 +192,53 @@ class Generator
         // DIR: \application\models\DbTable
         self::gen_dir(DIR_DBTABLE);
 
-        $result = self::query('SELECT * FROM ' . $table);
-        if (!$result):
-            exit('<div class="alert alert-error"><h4>Query failed!</h4> <a href="javascript:location.reload(true);" />Click here and try again</a></div>');
-        endif;
+        $fields = self::fetch_fields($database, $table);
 
-        $num_fields = self::num_fields($result);
+        $num_fields = count($fields);
+        if (!$num_fields)
+            exit('<div class="alert alert-error"><h4>No information available!</h4> <a href="javascript:location.reload(true);" />Click here and try again</a></div>');
 
-        $list = new ArrayIterator;
-        $process = null;
-        for ($i = 0; $i < $num_fields; $i++):
-            $meta = self::fetch_field($result, $i);
-            if (!$meta)
-                exit("No information available.<br />");
-            if ($i == 0)
-                $process[] = "<strong>Table Information</strong>: {$table}<br />";
+        $_list = new ArrayIterator;
+        $_listVars = $_listVOs = $primary_keys = $multiple_keys = null;
 
-            $process[] = "- <strong>{$meta->name}</strong>:<i>{$meta->type}</i> ";
-            if ($meta->primary_key)
-                $table_pk[] = $meta->name;
-            if ($meta->primary_key)
-                $process[] = "<strong style='color:green;'>[PK]</strong>"; // Primary key
-            if ($meta->multiple_key)
-                $process[] = "<strong style='color:blue;'>[FK]</strong>"; // Foreign key
-            if ($meta->not_null)
-                $process[] = "<strong style='color:red;'>[NN]</strong>"; // Not-null
+        $process[] = "<strong>Table Information</strong>: {$table}<br />";
+        foreach ($fields as $k => $val):
+            $_pk = $_fk = $_nn = null;
+            $name = $val['_name'];
+            $type = mb_strtolower($val['_type']);
 
-            $list[$i]['name'] = $meta->name;
-            $list[$i]['type'] = $meta->type == 'date' || $meta->type == 'datetime' ? 'date' : ($meta->numeric ? ($meta->not_null ? 'int' : 'null') : 'string');
+            if (preg_match('~(pri|1)~i', $val['_pkey'])):
+                $primary_keys[] = $name;
+                $_pk = "<strong style='color:green;'>[PK]</strong>"; // Primary key
+            endif;
 
-            $process[] = ($i == $num_fields - 1) ? "<br /><br />" : "<br />";
-        endfor;
+            if (preg_match('~(mul)~i', $val['_pkey'])):
+                $multiple_keys[] = $name;
+                $_fk = "<strong style='color:blue;'>[FK]</strong>"; // Foreign key
+            endif;
 
-        $listVars = null;
-        $listVOs = null;
-        for ($j = 0; $j < $num_fields; $j++):
-            $listVars.= self::gen_var($list[$j]);
-            $listVOs.= self::gen_vo($list[$j]);
-        endfor;
+            if (preg_match('~(no|1)~i', $val['_null'])):
+                $_nn = "<strong style='color:red;'>[NN]</strong>"; // Not-null
+            endif;
 
-        if (sizeof($table_pk) == 0):
-            $table_pk = "null";
-        elseif (sizeof($table_pk) == 1):
-            $table_pk = "'" . implode($table_pk) . "'";
+            $process[] = ('- <strong>' . $name . '</strong>:<i>' . $type . '</i> ' . $_pk . $_fk . $_nn . '<br />');
+
+            $_listVars.= self::gen_var($val);
+            $_listVOs.= self::gen_vo($val);
+        endforeach;
+
+        if (sizeof($primary_keys) == 0):
+            $primary_keys = "null";
+        elseif (sizeof($primary_keys) == 1):
+            $primary_keys = "'" . implode($primary_keys) . "'";
         else:
-            $listPKs = null;
+            $_listPKs = null;
+            if (sizeof($primary_keys))
+                foreach ($primary_keys as $var)
+                    $_listPKs.= ", '$var'";
+            $_listPKs = preg_replace('~, ~', '', $_listPKs, 1);
 
-            if (sizeof($table_pk))
-                foreach ($table_pk as $var)
-                    $listPKs.= ", '$var'";
-
-            $listPKs = preg_replace('~, ~', '', $listPKs, 1);
-
-            $table_pk = "array({$listPKs})";
+            $primary_keys = "array({$_listPKs})";
         endif;
 
         $tableNameUcwords = implode(array_map('ucwords', preg_split('~_~', $table)));
@@ -248,7 +253,7 @@ class Generator
 
 class Application_Model_{$tableNameUcwords} extends App_Model_Abstract
 {
-{$listVars}{$listVOs}
+{$_listVars}{$_listVOs}
 }
 MODEL;
 
@@ -293,32 +298,33 @@ DBTABLE;
 
         // Displays tables, data types and models created
         echo '<pre>';
-        foreach ($process as $ps):
+        foreach ($process as $ps)
             echo $ps;
-        endforeach;
+        echo '<br />';
 
-        // \models\File.php
+        // FILE: \models\File.php
         self::gen_file(DIR_MODELS, $tableNameUcwords, $content_model);
-        // \models\FileMapper.php
+        // FILE: \models\FileMapper.php
         self::gen_file(DIR_MODELS, $tableNameUcwords . 'Mapper', $content_mapper);
-        // \models\DbTable\File.php
+        // FILE: \models\DbTable\File.php
         self::gen_file(DIR_DBTABLE, $tableNameUcwords, $content_dbtable);
         echo '</pre>';
     }
 
     private static function gen_var($var)
     {
-        $name = strtolower($var['name']);
+        $name = strtolower($var['_name']);
         return "    protected \$_{$name};\n";
     }
 
     private static function gen_vo($var)
     {
-        $name = preg_split('~_~', $var['name']);
+        $name = preg_split('~_~', $var['_name']);
         $nameFirst = implode(array_map('ucwords', $name));
-        $nameLower = strtolower($var['name']);
-        $type = $var['type'];
-        $types = ($type == 'date') ? '$value;' : (($type == 'null') ? '(NULL !== $value) ? (int) $value : NULL;' : sprintf('(%s) $value;', $type));
+        $nameLower = strtolower($var['_name']);
+
+        $type = $var['_tpzf'];
+        $types = $type == 'date' ? '$value;' : ($type == 'null' ? '(NULL !== $value) ? (string) $value : NULL;' : sprintf('(%s) $value;', $type));
 
         $setMethod = <<<SETTER
 
